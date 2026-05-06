5ceccc970b147c7a03611f3b287a59380995cf42
user-synax <usersynax@gmail.com>
2026-05-06 13:56:36 +0530

    For loop in GO

---
+++ 5_for_loop/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	// i := 1
+	// for i <= 5 {
+	// 	fmt.Println(i)
+	// 	i += 1
+	// }
+
+	// OG Loop
+	// for i := 0; i < 5; i++ {
+	// 	fmt.Println(i)
+	// 	if i == 2 {
+	// 		continue
+	// 	}
+	// }
+
+	// Range
+	for i := range 11 {
+		fmt.Println(i)
+	}
+}
