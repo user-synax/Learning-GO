4747070bea2a99b39513ccc025ed4b9da4452e22
user-synax <usersynax@gmail.com>
2026-05-06 13:31:25 +0530

    Simple Values

---
+++ 2_simple-values/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(1)          // Int
+	fmt.Println("Hello Go") // String
+	fmt.Println("Hello Go") // String
+	fmt.Println(true)       // Bool
+	fmt.Println(3.14)       // Float
+
+	// Basic Arithmatic
+	fmt.Println(10 + 12)
+	fmt.Println(10 / 5)
+}
