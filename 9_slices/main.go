ee6f78a87fc509158240aa88bbe14e9ae32727f1
user-synax <usersynax@gmail.com>
2026-05-08 17:54:22 +0530

    Slices Basic Methods

--- 9_slices/main.go
+++ 9_slices/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 // import "slices"
 
@@ -12,8 +15,36 @@ func main() {
 	// fmt.Println(nums == nil)
 	// fmt.Println(len(nums))
 
-	var nums = make([]int, 1, 5)
-	nums = append(nums, 1, 2, 3, 4, 5, 6, 6, 7, 8, 9)
-	fmt.Println(nums)
-	fmt.Println(cap(nums))
+	// var nums = make([]int, 1, 5)
+	// nums = append(nums, 1, 2, 3, 4, 5, 6, 6, 7, 8, 9)
+	// fmt.Println(nums)
+	// fmt.Println(cap(nums))
+
+	// Copy function
+	// var numbers1 = make([]int, 0, 5)
+	// numbers1 = append(numbers1, 1,2,3,4,5)
+	// var numbers2 = make([]int, len(numbers1))
+	// copy(numbers2, numbers1)
+	// fmt.Println(numbers1)
+	// fmt.Println(numbers2)
+
+	// Slice operator
+
+	// var nums = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	// fmt.Println(nums[0:5])
+	// fmt.Println(nums[0:])
+	// fmt.Println(nums[:5])
+
+	// Slice package
+	var nums1 = []int{1, 2}
+	var nums2 = []int{1, 2}
+
+	fmt.Println(slices.Equal(nums1, nums2))
+	fmt.Println(slices.Compare(nums1, nums2))
+	fmt.Println(slices.Contains(nums1, 3))
+
+	numbers := []int{1,2,7,7,3,2,5,8,5,2,4,5,7}
+	slices.Sort(numbers)
+	fmt.Println(numbers)
+
 }
