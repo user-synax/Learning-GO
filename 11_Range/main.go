a9525d30e25d54d80c2dd8c517c7ec3d04f5a40a
user-synax <usersynax@gmail.com>
2026-05-08 18:39:33 +0530

    Range in GO

---
+++ 11_Range/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{1, 2, 3}
+
+	// Normal method to iterate over a slice
+	// for i := 0; i < len(nums); i++ {
+	// 	fmt.Println(nums[i]+1)
+	// }
+
+	// using range
+	sum := 0
+	for index, num := range nums {
+		sum += num
+		fmt.Println("Index",index, "Value",num)
+	}
+	fmt.Println("Total sum:", sum)
+
+	m := map[string]string{
+		"fname":"Jhon",
+		"lname":"Doe",
+	}
+
+	for k, v := range m {
+		fmt.Println(k, ":", v)
+	}
+
+}
